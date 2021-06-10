d75091b342bc3bb28aaa2452cd9b0b7bb48bfc6d
박성빈 <kled@kakao.com>
2021-06-10 23:00:19 +0900

    fix .

---
+++ example-a/src/main/java/com/example/a/ExampleAApplication.java
@@ -0,0 +1,13 @@
+package com.example.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleAApplication.class, args);
+    }
+
+}
---
+++ example-b/src/main/java/com/example/b/ExampleBApplication.java
@@ -0,0 +1,13 @@
+package com.example.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBApplication.class, args);
+    }
+
+}
